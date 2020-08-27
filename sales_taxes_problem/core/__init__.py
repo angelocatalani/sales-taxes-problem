726d21ba53f349ae069062ebd1808ca97326a147
angelobs <anc@bendingspoons.com>
2020-08-27 21:00:32 +0200

    feat(core): implement exercise item class with tests

--- sales_taxes_problem/core/__init__.py
+++ sales_taxes_problem/core/__init__.py
@@ -1,3 +1,4 @@
 __all__ = ["Basket", "ExerciseItem", "ExerciseTaxCalculator"]
 from sales_taxes_problem.core.basket import Basket
-from sales_taxes_problem.core.exercise_item import ExerciseItem, ExerciseTaxCalculator
+from sales_taxes_problem.core.exercise_item import (ExerciseItem,
+                                                    ExerciseTaxCalculator)
