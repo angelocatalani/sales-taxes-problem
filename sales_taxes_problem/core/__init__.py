738bdbb6ff2c5ce2aa3282385d34fba5433ba61a
Angelo Catalani <catalaniangelo@gmail.com>
2020-08-27 21:15:24 +0200

    feat(core): implement exercise item class with tests (#10)

--- sales_taxes_problem/core/__init__.py
+++ sales_taxes_problem/core/__init__.py
@@ -1,2 +1,3 @@
-__all__ = ["Basket"]
+__all__ = ["Basket", "ExerciseItem", "ExerciseTaxCalculator"]
 from sales_taxes_problem.core.basket import Basket
+from sales_taxes_problem.core.exercise_item import ExerciseItem, ExerciseTaxCalculator
--- tests/core/test_basket.py
+++ tests/core/test_basket.py
@@ -4,7 +4,7 @@ from unittest.mock import MagicMock, patch
 import pytest
 
 from sales_taxes_problem.core import Basket
-from tests.core.helpers import BASKET_NUMBER, ItemInfo
+from tests.core.helpers import BASKET_NUMBER, ItemTaxInfo
 
 
 def test_basket_init() -> None:
@@ -18,14 +18,14 @@ def test_basket_init() -> None:
 @pytest.mark.parametrize(
     "items_info,total_taxes",
     [
-        ([ItemInfo(taxes=12, quantity=2)], 24.0),
-        ([ItemInfo(taxes=12, quantity=2), ItemInfo(taxes=12.35, quantity=1)], 36.35),
-        ([ItemInfo(taxes=12, quantity=2), ItemInfo(taxes=12.35, quantity=0)], 24.0),
-        ([ItemInfo(taxes=0.35, quantity=2), ItemInfo(taxes=1.5, quantity=2)], 3.70),
+        ([ItemTaxInfo(taxes=12, quantity=2)], 24.0),
+        ([ItemTaxInfo(taxes=12, quantity=2), ItemTaxInfo(taxes=12.35, quantity=1)], 36.35),
+        ([ItemTaxInfo(taxes=12, quantity=2), ItemTaxInfo(taxes=12.35, quantity=0)], 24.0),
+        ([ItemTaxInfo(taxes=0.35, quantity=2), ItemTaxInfo(taxes=1.5, quantity=2)], 3.70),
         ([], 0.0),
     ],
 )  # type: ignore[misc]
-def test_basket_get_total_taxes(items_info: List[ItemInfo], total_taxes: int) -> None:
+def test_basket_get_total_taxes(items_info: List[ItemTaxInfo], total_taxes: int) -> None:
     """
     Test the taxes are correctly computed.
     """
@@ -43,18 +43,18 @@ def test_basket_get_total_taxes(items_info: List[ItemInfo], total_taxes: int) ->
 @pytest.mark.parametrize(
     "items_info,final_price",
     [
-        ([ItemInfo(taxes=12, quantity=2, price=100)], 224.0),
+        ([ItemTaxInfo(taxes=12, quantity=2, price=100)], 224.0),
         (
             [
-                ItemInfo(taxes=12, quantity=2, price=123.35),
-                ItemInfo(taxes=12.35, quantity=1, price=50.75),
+                ItemTaxInfo(taxes=12, quantity=2, price=123.35),
+                ItemTaxInfo(taxes=12.35, quantity=1, price=50.75),
             ],
             333.80,
         ),
         ([], 0),
     ],
 )  # type: ignore[misc]
-def test_basket_get_final_price(items_info: List[ItemInfo], final_price: int) -> None:
+def test_basket_get_final_price(items_info: List[ItemTaxInfo], final_price: int) -> None:
     """
     Test the taxes are correctly computed.
     """
