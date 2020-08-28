b323ec42c61030f83135e5fc4bccd7271a3b506d
angelobs <anc@bendingspoons.com>
2020-08-28 21:29:12 +0200

    refactor(code): separate item and tax_calculator

--- sales_taxes_problem/common/item.py
+++ sales_taxes_problem/common/item.py
@@ -1,24 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import Any
 
-
-class TaxCalculator(ABC):
-    """
-    Computes the taxes under a given taxation system.
-    """
-
-    @abstractmethod
-    def compute_tax(self, item_name: str, item_price: float) -> float:
-        """
-        Compute the taxes for the input Item's name and price rounded up to the nearest 0.05.
-
-        :param item_name: the item's name
-        :param item_price: the item's price
-
-        :return: the taxes for the item_name
-        """
-
-        raise NotImplementedError()  # pragma: no cover
+from sales_taxes_problem.common.tax_calculator import TaxCalculator
 
 
 class Item(ABC):
