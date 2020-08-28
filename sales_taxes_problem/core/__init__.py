32f46c1c57eff81ae69cc8554d249c421c6c044a
Angelo Catalani <catalaniangelo@gmail.com>
2020-08-28 22:04:20 +0200

    feat: improve item classification (#21)

--- sales_taxes_problem/core/__init__.py
+++ sales_taxes_problem/core/__init__.py
@@ -1,4 +1,5 @@
 __all__ = ["ExerciseItem", "ExerciseParser", "ExerciseTaxCalculator"]
 
-from sales_taxes_problem.core.exercise_item import ExerciseItem, ExerciseTaxCalculator
+from sales_taxes_problem.core.exercise_item import ExerciseItem
 from sales_taxes_problem.core.exercise_parser import ExerciseParser
+from sales_taxes_problem.core.exercise_tax_calculator import ExerciseTaxCalculator
